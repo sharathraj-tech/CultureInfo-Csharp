cef9458bbe989a8fb8cad6e8ddb57ea108b8d98d
Sharath Raj <sharathrj143@gmail.com>
2022-09-01 22:25:10 +0530

    Add files via upload

---
+++ CultureInfoTest/CultureInfoTest/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace CultureInfoTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTextToConvert = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtVariant = new System.Windows.Forms.TextBox();
+            this.txtResultView = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTextToConvert
+            // 
+            this.txtTextToConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTextToConvert.Location = new System.Drawing.Point(198, 73);
+            this.txtTextToConvert.Multiline = true;
+            this.txtTextToConvert.Name = "txtTextToConvert";
+            this.txtTextToConvert.Size = new System.Drawing.Size(386, 34);
+            this.txtTextToConvert.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please Enter Value";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(590, 73);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(101, 34);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(181, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(258, 28);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select Language";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(478, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Or Varient";
+            // 
+            // txtVariant
+            // 
+            this.txtVariant.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVariant.Location = new System.Drawing.Point(611, 19);
+            this.txtVariant.Multiline = true;
+            this.txtVariant.Name = "txtVariant";
+            this.txtVariant.Size = new System.Drawing.Size(169, 27);
+            this.txtVariant.TabIndex = 3;
+            // 
+            // txtResultView
+            // 
+            this.txtResultView.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultView.Location = new System.Drawing.Point(15, 139);
+            this.txtResultView.Multiline = true;
+            this.txtResultView.Name = "txtResultView";
+            this.txtResultView.Size = new System.Drawing.Size(765, 124);
+            this.txtResultView.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(697, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(792, 281);
+            this.Controls.Add(this.txtResultView);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtVariant);
+            this.Controls.Add(this.txtTextToConvert);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ui Culture Check on Currency";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTextToConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtVariant;
+        private System.Windows.Forms.TextBox txtResultView;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CultureInfoTest/CultureInfoTest/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CultureInfoTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string strConvertType = string.Empty;
+            try
+            {
+                if (txtVariant.Text != "")
+                {
+                    strConvertType = txtVariant.Text;
+                }
+                else
+                {
+                    if (comboBox1.SelectedIndex != -1)
+                    {
+                        strConvertType = comboBox1.SelectedValue.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please Select Culture Info!");
+                    }
+                }
+                if (txtTextToConvert.Text != "")
+                {
+                    CultureInfo c = new CultureInfo(strConvertType);
+
+                    double a = Convert.ToDouble(txtTextToConvert.Text, c);
+
+                    txtResultView.Text = "String formatted for the " + c.Name + " culture: " + Environment.NewLine + a.ToString("C", c);
+                }
+                else
+                {
+                    MessageBox.Show("Please enter Numbers to convert!");
+                    txtTextToConvert.Focus();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("ERROR: "+ex.Message);
+            }
+            
+            
+        }
+
+        public string ToCurrencyFormat(string str)
+        {
+            string result = "";
+            try
+            {
+                result = Convert.ToDouble(str, System.Globalization.CultureInfo.InvariantCulture).ToString("C");
+            }
+            catch { }
+            return result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                var items = new List<Item>();
+                items.Add(new Item("---SELECT Culture---", ""));
+                CultureInfo[] cinfo = CultureInfo.GetCultures(CultureTypes.AllCultures & ~CultureTypes.NeutralCultures);
+                    foreach (CultureInfo a in cinfo)
+                    {
+                        if (!string.IsNullOrEmpty(a.Name))
+                        {
+                            items.Add(new Item(a.DisplayName, a.Name));
+                        }
+                    }
+                comboBox1.DisplayMember = "Name";
+                comboBox1.ValueMember = "Value";
+                comboBox1.DataSource = items;
+                //getTableData(strSQL);
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(this, "Something Went Wrong!\n REASON: " + ee.Message, "ERROR", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+            txtTextToConvert.Focus();
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex != -1)
+                txtVariant.Text = comboBox1.SelectedValue.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtTextToConvert.Text = "";
+            txtResultView.Text = "";
+            txtVariant.Text = "";
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; private set; }
+        public string Value { get; private set; }
+        public Item(string _name, string _value)
+        {
+            Name = _name; Value = _value;
+        }
+    }
+}
